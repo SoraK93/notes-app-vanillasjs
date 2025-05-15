2faef1fbcd7c90eaf709278467f29a04dddcb357
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-05-15 06:34:51 +0530

    ADD PIN, DELETE AND ARCHIVE FEATURE

--- app.js
+++ app.js
@@ -12,25 +12,23 @@
  */
 export const renderNotes = (notes) => {
   let newNotes = notes.map(({ id, note, title, isPinned, isArchived }) => {
-    return (
-      `<div class="single-note shadow">
+    return `<div class="single-note shadow">
         <div class="d-flex align-center title-container">
           <span class="single-note-title">${title}</span>
-          <button class="button del-btn v-hidden">
-            <span class="material-icons-outlined">delete</span>
+          <button class="button del-btn v-hidden" data-type="del" data-id="${id}">
+            <span class="material-icons-outlined" data-type="del" data-id="${id}">delete</span>
           </button>
         </div>
         <p>${note}</p>
         <div class="options d-flex gap-md">
-          <button class="button btn pinned-btn v-hidden">
-            <span class="material-icons-outlined">push_pin</span>
+          <button class="button btn pinned-btn v-hidden" data-id="${id}" data-type="pinned">
+            <span class=${isPinned ? "material-icons" : "material-icons-outlined"} data-id="${id}" data-type="pinned">push_pin</span>
           </button>
-          <button class="button btn pinned-btn v-hidden">
-            <span class="material-icons-outlined">archive</span>
+          <button class="button btn pinned-btn v-hidden" data-id="${id}" data-type="archive">
+            <span class="material-icons-outlined" data-id="${id}" data-type="archive">archive</span>
           </button>
         </div>
-      </div>`
-    );
+      </div>`;
   });
   newNotes = newNotes.join("");
   return newNotes;
--- main.js
+++ main.js
@@ -6,9 +6,21 @@ let addNoteButton = document.querySelector(".add-btn");
 let notesDisplay = document.querySelector(".notes-display");
 let showOtherNotes = document.querySelector(".notes-container");
 let showPinnedNotes = document.querySelector(".pinned-notes-container");
+let pinnedNoteTitle = document.querySelector(".pin-title");
+let otherNoteTitle = document.querySelector(".other-title");
 
 /** Loads up data from localStorage else create a empty array */
 let arrayOfNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+/** Functionality for rendering all the notes archived, pinned or anything else. Takes in a array of notes. */
+function renderPinnedArchiveNotes(notesArray) {
+  showPinnedNotes.innerHTML = renderNotes(
+    notesArray.filter(({ isPinned, isArchived }) => isPinned && !isArchived)
+  );
+  showOtherNotes.innerHTML = renderNotes(
+    notesArray.filter(({ isPinned, isArchived }) => !isPinned && !isArchived)
+  );
+}
 
 /** Add new notes */
 addNoteButton.addEventListener("click", () => {
@@ -23,10 +35,45 @@ addNoteButton.addEventListener("click", () => {
         isArchived: false,
       },
     ];
-    showOtherNotes.innerHTML = renderNotes(arrayOfNotes);
-    localStorage.setItem("notes", JSON.stringify(arrayOfNotes));
+    note.value = title.value = "";
+    renderPinnedArchiveNotes(arrayOfNotes);
   }
 });
 
-/** Renders all the notes saved inside localStorage */
-showOtherNotes.innerHTML = renderNotes(arrayOfNotes);
+notesDisplay.addEventListener("click", (event) => {
+  let type = event.target.dataset.type;
+  let noteId = event.target.dataset.id;
+
+  switch (type) {
+    case "del":
+      arrayOfNotes = arrayOfNotes.filter(({ id }) => id.toString() !== noteId);
+      renderPinnedArchiveNotes(arrayOfNotes);
+      localStorage.setItem("notes", JSON.stringify(notesArray));
+      break;
+    case "pinned":
+      // Reversing isPinned value of the target note
+      arrayOfNotes = arrayOfNotes.map((note) =>
+        note.id.toString() === noteId
+          ? { ...note, isPinned: !note.isPinned }
+          : note
+      );
+      renderPinnedArchiveNotes(arrayOfNotes);
+      localStorage.setItem("notes", JSON.stringify(notesArray));
+      break;
+    case "archive":
+      arrayOfNotes = arrayOfNotes.map((note) =>
+        note.id.toString() === noteId
+          ? { ...note, isArchived: !note.isArchived }
+          : note
+      );
+      renderPinnedArchiveNotes(arrayOfNotes);
+      localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+});
+
+if (arrayOfNotes.length > 0) {
+  pinnedNoteTitle.classList.toggle("d-none");
+  otherNoteTitle.classList.toggle("d-none");
+}
+
+renderPinnedArchiveNotes(arrayOfNotes);
